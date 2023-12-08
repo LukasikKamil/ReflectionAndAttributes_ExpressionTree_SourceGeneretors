acd5b341f3b2f13f5c81dd456bc5d6b95e5bc790
Kamil Łukasik <poczta.lukasik@gmail.com>
2023-12-08 13:43:40 +0100

    #SourceGenerator

---
+++ GeneratingCodeLib/MessageSourceGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis; // [Generator], GeneratorInitializationContext
+                              // ISourceGenerator, GeneratorExecutionContext
+
+namespace ALLinONE.Shared;
+
+[Generator]
+public class MessageSourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext execContext)
+    {
+        IMethodSymbol mainMethod = execContext.Compilation.GetEntryPoint(execContext.CancellationToken);
+
+        string sourceCode = $@"// source-generated code
+        static partial class {mainMethod.ContainingType.Name}
+        {{
+            static partial void Message(string message)
+            {{
+                System.Console.WriteLine($""Generator says: '{{message}}'"");
+            }}
+        }}
+        ";
+        string typeName = mainMethod.ContainingType.Name;
+        execContext.AddSource($"{typeName}.Methods.g.cs", sourceCode);
+    }
+
+    public void Initialize(GeneratorInitializationContext initContext)
+    {
+        // this source generator does not need any initialization
+    }
+}
